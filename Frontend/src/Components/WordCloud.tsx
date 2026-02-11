f51f8ccab908139173b2e8058c9fea0911098f97
Elijah Kelting <elijahakbusiness@gmail.com>
2026-02-10 19:22:44 -0800

    Updated cloud shape
    Made the cloud shape more of an oval to reflect more of a cloud.

--- Frontend/src/Components/WordCloud.tsx
+++ Frontend/src/Components/WordCloud.tsx
@@ -4,24 +4,27 @@ import { weightToColor } from "../Utilities/Utils";
 
 type WordCloudProps = {
   words: WordType[];
-  radius?: number;
+  radiusX?: number;
+  radiusY?: number;
+  radiusZ?: number;
 };
 
-export default function WordCloud({ words, radius = 5 }: WordCloudProps) {
+export default function WordCloud({ words, radiusX = 5, radiusY = 3, radiusZ = 1.5 }: WordCloudProps) {
 
   const minWeight = Math.min(...words.map(w => w.weight));
   const maxWeight = Math.max(...words.map(w => w.weight));
 
-  
+
   return (
     <>
       {words.map((w, i) => {
         // random position on a sphere
         const theta = Math.random() * Math.PI * 2;
         const phi = Math.acos(2 * Math.random() - 1);
-        const x = radius * Math.sin(phi) * Math.cos(theta);
-        const y = radius * Math.sin(phi) * Math.sin(theta);
-        const z = radius * Math.cos(phi);
+
+        const x = radiusX * Math.sin(phi) * Math.cos(theta);
+        const y = radiusY * Math.sin(phi) * Math.sin(theta);
+        const z = radiusZ;
 
         return <Word key={i} word={w.word} weight={w.weight} position={[x, y, z]} color={weightToColor(w.weight, minWeight, maxWeight)} />;
       })}
